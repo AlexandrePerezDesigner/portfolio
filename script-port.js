b78948b012b035752b47ed983b497219d5914db0
AlexandrePerezDesigner <alexandreperezdesign@gmail.com>
2024-12-02 15:24:25 -0300

    Add files via upload

--- script-port.js
+++ script-port.js
@@ -1,89 +1,302 @@
-const images = [
-            { src: 'portfolio/01net1.jpg', title: 'Projeto 1', description: 'Descrição do projeto 1.' },
-            { src: 'assets/p2.jpg', title: 'Projeto 2', description: 'Descrição do projeto 2.' },
-            { src: 'assets/p3.jpg', title: 'Projeto 3', description: 'Descrição do projeto 3.' },
-            { src: 'assets/p4.jpg', title: 'Projeto 4', description: 'Descrição do projeto 4.' },
-            { src: 'assets/p5.jpg', title: 'Projeto 5', description: 'Descrição do projeto 5.' },
-            { src: 'assets/p6.jpg', title: 'Projeto 6', description: 'Descrição do projeto 6.' },
-
-            { src: 'assets/p1.jpg', title: 'Vídeo 1', description: 'Descrição do vídeo 1.' },
-            { src: 'assets/p2.jpg', title: 'Vídeo 2', description: 'Descrição do vídeo 2.' },
-            { src: 'assets/p3.jpg', title: 'Vídeo 3', description: 'Descrição do vídeo 3.' },
-            { src: 'assets/p4.jpg', title: 'Vídeo 4', description: 'Descrição do vídeo 4.' },
-            { src: 'assets/p5.jpg', title: 'Vídeo 5', description: 'Descrição do vídeo 5.' },
-            { src: 'assets/p6.jpg', title: 'Vídeo 6', description: 'Descrição do vídeo 6.' },
-
-            { src: 'assets/p1.jpg', title: 'Graphic 1', description: 'Descrição do gráfico 1.' },
-            { src: 'assets/p2.jpg', title: 'Graphic 2', description: 'Descrição do gráfico 2.' },
-            { src: 'assets/p3.jpg', title: 'Graphic 3', description: 'Descrição do gráfico 3.' },
-            { src: 'assets/p4.jpg', title: 'Graphic 4', description: 'Descrição do gráfico 4.' },
-            { src: 'assets/p5.jpg', title: 'Graphic 5', description: 'Descrição do gráfico 5.' },
-            { src: 'assets/p6.jpg', title: 'Graphic 6', description: 'Descrição do gráfico 6.' },
-
-            { src: 'assets/p1.jpg', title: 'Branding 1', description: 'Descrição do branding 1.' },
-            { src: 'assets/p2.jpg', title: 'Branding 2', description: 'Descrição do branding 2.' },
-            { src: 'assets/p3.jpg', title: 'Branding 3', description: 'Descrição do branding 3.' },
-            { src: 'assets/p4.jpg', title: 'Branding 4', description: 'Descrição do branding 4.' },
-            { src: 'assets/p5.jpg', title: 'Branding 5', description: 'Descrição do branding 5.' },
-            { src: 'assets/p6.jpg', title: 'Branding 6', description: 'Descrição do branding 6.' },
-
-            { src: 'assets/p1.jpg', title: 'Emkt 1', description: 'Descrição do Emkt 1.' },
-            { src: 'assets/p2.jpg', title: 'Emkt 2', description: 'Descrição do Emkt 2.' },
-            { src: 'assets/p3.jpg', title: 'Emkt 3', description: 'Descrição do Emkt 3.' },
-            { src: 'assets/p4.jpg', title: 'Emkt 4', description: 'Descrição do Emkt 4.' },
-            { src: 'assets/p5.jpg', title: 'Emkt 5', description: 'Descrição do Emkt 5.' },
-            { src: 'assets/p6.jpg', title: 'Emkt 6', description: 'Descrição do Emkt 6.' },
-
-            { src: 'assets/p1.jpg', title: 'Banner 1', description: 'Descrição do Banner 1.' },
-            { src: 'assets/p2.jpg', title: 'Banner 2', description: 'Descrição do Banner 2.' },
-            { src: 'assets/p3.jpg', title: 'Banner 3', description: 'Descrição do Emkt 3.' },
-            { src: 'assets/p4.jpg', title: 'Banner 4', description: 'Descrição do Emkt 4.' },
-            { src: 'assets/p5.jpg', title: 'Banner 5', description: 'Descrição do Emkt 5.' },
-            { src: 'assets/p6.jpg', title: 'Banner 6', description: 'Descrição do Emkt 6.' }
+const projects = [
+    {
+        id: 1,
+        images: [
+            { src: 'portfolio/01net1.jpg', title: 'Net Combo - Imagem 1', description: 'Descrição do projeto Net Combo - Imagem 1.' },
+            { src: 'portfolio/01net2.jpg', title: 'Net Combo - Imagem 2', description: 'Descrição do projeto Net Combo - Imagem 2.' }
+        ]
+    },
+    {
+        id: 2,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 2', description: 'Descrição do projeto 2.' }
+        ]
+    },
+    {
+        id: 3,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 3', description: 'Descrição do projeto 3.' }
+        ]
+    },
+    {
+        id: 4,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 4', description: 'Descrição do projeto 4.' }
+        ]
+    },
+    {
+        id: 5,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 5', description: 'Descrição do projeto 5.' }
+        ]
+    },
+    {
+        id: 6,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 6', description: 'Descrição do projeto 6.' }
+        ]
+    },
+    {
+        id: 7,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 7', description: 'Descrição do projeto 7.' }
+        ]
+    },
+    {
+        id: 8,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 8', description: 'Descrição do projeto 8.' }
+        ]
+    },
+    {
+        id: 9,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 9', description: 'Descrição do projeto 9.' }
+        ]
+    },
+    {
+        id: 10,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 10', description: 'Descrição do projeto 10.' }
+        ]
+    },
+    {
+        id: 11,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 11', description: 'Descrição do projeto 11.' }
+        ]
+    },
+    {
+        id: 12,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 12', description: 'Descrição do projeto 12.' }
+        ]
+    },
+    {
+        id: 13,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 13', description: 'Descrição do projeto 13.' }
+        ]
+    },
+    {
+        id: 14,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 14', description: 'Descrição do projeto 14.' }
+        ]
+    },
+    {
+        id: 15,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 15', description: 'Descrição do projeto 15.' }
+        ]
+    },
+    {
+        id: 16,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 16', description: 'Descrição do projeto 16.' }
+        ]
+    },
+    {
+        id: 17,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 17', description: 'Descrição do projeto 17.' }
+        ]
+    },
+    {
+        id: 18,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 18', description: 'Descrição do projeto 18.' }
+        ]
+    },
+    {
+        id: 19,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 19', description: 'Descrição do projeto 19.' }
+        ]
+    },
+    {
+        id: 20,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 20', description: 'Descrição do projeto 20.' }
+        ]
+    },
+    {
+        id: 21,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 21', description: 'Descrição do projeto 21.' }
+        ]
+    },
+    {
+        id: 22,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 22', description: 'Descrição do projeto 22.' }
+        ]
+    },
+    {
+        id: 23,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 23', description: 'Descrição do projeto 23.' }
+        ]
+    },
+    {
+        id: 24,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 24', description: 'Descrição do projeto 24.' }
+        ]
+    },
+    {
+        id: 25,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 25', description: 'Descrição do projeto 25.' }
+        ]
+    },
+    {
+        id: 26,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 26', description: 'Descrição do projeto 26.' }
+        ]
+    },
+    {
+        id: 27,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 27', description: 'Descrição do projeto 27.' }
+        ]
+    },
+    {
+        id: 28,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 28', description: 'Descrição do projeto 28.' }
+        ]
+    },
+    {
+        id: 29,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 29', description: 'Descrição do projeto 29.' }
+        ]
+    },
+    {
+        id: 30,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 30', description: 'Descrição do projeto 30.' }
+        ]
+    },
+    {
+        id: 31,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 31', description: 'Descrição do projeto 31.' }
+        ]
+    },
+    {
+        id: 32,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 32', description: 'Descrição do projeto 32.' }
+        ]
+    },
+    {
+        id: 33,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 33', description: 'Descrição do projeto 33.' }
+        ]
+    },
+    {
+        id: 34,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 34', description: 'Descrição do projeto 34.' }
+        ]
+    },
+    {
+        id: 35,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 35', description: 'Descrição do projeto 35.' }
+        ]
+    },
+    {
+        id: 36,
+        images: [
+            { src: 'assets/p2.jpg', title: 'Projeto 36', description: 'Descrição do projeto 36.' }
+        ]
+    }
 ];
 
+// Variáveis globais para controlar o índice da imagem atual e o projeto atual
 let currentImageIndex = 0;
+let currentProject = null;
 
-function openPopup(index) {
-    currentImageIndex = index - 1;
-    updatePopupContent();
+// Função para abrir o popup
+function openPopup(projectId) {
+    // Encontre o projeto pelo ID
+    currentProject = projects.find(p => p.id === projectId);
+    if (!currentProject) return;
+
+    currentImageIndex = 0;
+
+    // Carregar a primeira imagem no popup
+    updatePopupContent(currentProject.images[currentImageIndex]);
+
+    // Mostrar o popup
     const popup = document.getElementById('popup');
     popup.classList.add('show');
+
+    // Mostrar ou esconder as setas de navegação conforme necessário
+    const navigation = document.querySelector('.navigation');
+    if (currentProject.images.length > 1) {
+        navigation.style.display = 'flex';
+    } else {
+        navigation.style.display = 'none';
+    }
 }
 
+// Função para atualizar o conteúdo do popup
+function updatePopupContent(image) {
+    const popupImage = document.getElementById('popupImg');
+    const popupTitle = document.getElementById('popupTitle');
+    const popupDescription = document.getElementById('popupDescription');
+
+    // Atualizar os elementos do popup com a imagem atual
+    popupImage.src = image.src;
+    popupTitle.textContent = image.title;
+    popupDescription.textContent = image.description;
+
+    // Adicionar evento de clique para expandir a imagem
+    popupImage.onclick = openExpandedPopup;
+}
+
+// Funções de navegação para próxima e anterior imagem
+function prevImage() {
+    if (currentImageIndex > 0) {
+        currentImageIndex--;
+        updatePopupContent(currentProject.images[currentImageIndex]);
+    }
+}
+
+function nextImage() {
+    if (currentImageIndex < currentProject.images.length - 1) {
+        currentImageIndex++;
+        updatePopupContent(currentProject.images[currentImageIndex]);
+    }
+}
+
+// Função para fechar o popup
 function closePopup() {
     const popup = document.getElementById('popup');
     popup.classList.remove('show');
 }
 
-function prevImage() {
-    currentImageIndex = (currentImageIndex === 0) ? images.length - 1 : currentImageIndex - 1;
-    updatePopupContent();
-}
-
-function nextImage() {
-    currentImageIndex = (currentImageIndex === images.length - 1) ? 0 : currentImageIndex + 1;
-    updatePopupContent();
-}
-
-function updatePopupContent() {
-    document.getElementById('popupImg').src = images[currentImageIndex].src;
-    document.getElementById('popupTitle').innerText = images[currentImageIndex].title;
-    document.getElementById('popupDescription').innerText = images[currentImageIndex].description;
-}
-
+// Função para abrir o popup expandido
 function openExpandedPopup() {
     const expandedPopup = document.getElementById('expandedPopup');
     const expandedImg = document.getElementById('expandedImg');
-    expandedImg.src = images[currentImageIndex].src;
+    expandedImg.src = currentProject.images[currentImageIndex].src;
     expandedPopup.classList.add('show');
 }
 
+// Função para fechar o popup expandido
 function closeExpandedPopup() {
     const expandedPopup = document.getElementById('expandedPopup');
     expandedPopup.classList.remove('show');
 }
 
+// Função para alternar entre as seções do portfólio
 function showSection(sectionId) {
     // Obter a seção atualmente visível
     const currentSection = document.querySelector('.portfolio.show');
