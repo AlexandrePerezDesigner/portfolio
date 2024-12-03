449169cbaa8df1b395190a9cdb7a5308feb61a99
AlexandrePerezDesigner <alexandreperezdesign@gmail.com>
2024-12-03 09:42:40 -0300

    Add files via upload

--- script-port.js
+++ script-port.js
@@ -1,226 +1,19 @@
-const projects = [
-    {
-        id: 1,
-        images: [
-            { src: 'portfolio/01net1.jpg', title: 'Net Combo - Landing Page', description: 'Esta landing page promove o pacote Net Combo, destacando internet, TV por assinatura e telefone fixo. A primeira tela apresenta a oferta de 30 Mega por R$ 59,90 ao mês, com botões Verificar Disponibilidade. Ao clicar, o usuário é levado para uma segunda tela, onde insere seus dados para checar a disponibilidade do serviço. O design é direto, com cores vibrantes e botões estrategicamente posicionados para incentivar a conversão, facilitando o entendimento dos benefícios e a adesão ao serviço.' },
-            { src: 'portfolio/01net2.jpg', title: 'Net Combo - Landing Page', description: 'Descrição do projeto Net Combo - Imagem 2.' }
-        ]
-    },
-    {
-        id: 2,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 2', description: 'Descrição do projeto 2.' }
-        ]
-    },
-    {
-        id: 3,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 3', description: 'Descrição do projeto 3.' }
-        ]
-    },
-    {
-        id: 4,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 4', description: 'Descrição do projeto 4.' }
-        ]
-    },
-    {
-        id: 5,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 5', description: 'Descrição do projeto 5.' }
-        ]
-    },
-    {
-        id: 6,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 6', description: 'Descrição do projeto 6.' }
-        ]
-    },
-    {
-        id: 7,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 7', description: 'Descrição do projeto 7.' }
-        ]
-    },
-    {
-        id: 8,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 8', description: 'Descrição do projeto 8.' }
-        ]
-    },
-    {
-        id: 9,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 9', description: 'Descrição do projeto 9.' }
-        ]
-    },
-    {
-        id: 10,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 10', description: 'Descrição do projeto 10.' }
-        ]
-    },
-    {
-        id: 11,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 11', description: 'Descrição do projeto 11.' }
-        ]
-    },
-    {
-        id: 12,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 12', description: 'Descrição do projeto 12.' }
-        ]
-    },
-    {
-        id: 13,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 13', description: 'Descrição do projeto 13.' }
-        ]
-    },
-    {
-        id: 14,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 14', description: 'Descrição do projeto 14.' }
-        ]
-    },
-    {
-        id: 15,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 15', description: 'Descrição do projeto 15.' }
-        ]
-    },
-    {
-        id: 16,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 16', description: 'Descrição do projeto 16.' }
-        ]
-    },
-    {
-        id: 17,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 17', description: 'Descrição do projeto 17.' }
-        ]
-    },
-    {
-        id: 18,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 18', description: 'Descrição do projeto 18.' }
-        ]
-    },
-    {
-        id: 19,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 19', description: 'Descrição do projeto 19.' }
-        ]
-    },
-    {
-        id: 20,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 20', description: 'Descrição do projeto 20.' }
-        ]
-    },
-    {
-        id: 21,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 21', description: 'Descrição do projeto 21.' }
-        ]
-    },
-    {
-        id: 22,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 22', description: 'Descrição do projeto 22.' }
-        ]
-    },
-    {
-        id: 23,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 23', description: 'Descrição do projeto 23.' }
-        ]
-    },
-    {
-        id: 24,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 24', description: 'Descrição do projeto 24.' }
-        ]
-    },
-    {
-        id: 25,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 25', description: 'Descrição do projeto 25.' }
-        ]
-    },
-    {
-        id: 26,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 26', description: 'Descrição do projeto 26.' }
-        ]
-    },
-    {
-        id: 27,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 27', description: 'Descrição do projeto 27.' }
-        ]
-    },
-    {
-        id: 28,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 28', description: 'Descrição do projeto 28.' }
-        ]
-    },
-    {
-        id: 29,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 29', description: 'Descrição do projeto 29.' }
-        ]
-    },
-    {
-        id: 30,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 30', description: 'Descrição do projeto 30.' }
-        ]
-    },
-    {
-        id: 31,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 31', description: 'Descrição do projeto 31.' }
-        ]
-    },
-    {
-        id: 32,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 32', description: 'Descrição do projeto 32.' }
-        ]
-    },
-    {
-        id: 33,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 33', description: 'Descrição do projeto 33.' }
-        ]
-    },
-    {
-        id: 34,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 34', description: 'Descrição do projeto 34.' }
-        ]
-    },
-    {
-        id: 35,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 35', description: 'Descrição do projeto 35.' }
-        ]
-    },
-    {
-        id: 36,
-        images: [
-            { src: 'assets/p2.jpg', title: 'Projeto 36', description: 'Descrição do projeto 36.' }
-        ]
-    }
-];
-
-// Variáveis globais para controlar o índice da imagem atual e o projeto atual
 let currentImageIndex = 0;
 let currentProject = null;
+
+// Função para carregar os dados do JSON
+function loadProjectsJSON() {
+    fetch('projects.json') // Nome do arquivo JSON
+        .then(response => response.json())
+        .then(data => {
+            // Definir os dados dos projetos
+            window.projects = data.projects;
+        })
+        .catch(error => console.error("Erro ao carregar o JSON:", error));
+}
+
+// Chamar a função para carregar o JSON quando a página for carregada
+document.addEventListener("DOMContentLoaded", loadProjectsJSON);
 
 // Função para abrir o popup
 function openPopup(projectId) {
@@ -237,7 +30,7 @@ function openPopup(projectId) {
     const popup = document.getElementById('popup');
     popup.classList.add('show');
 
-    // Mostrar ou esconder as setas de navegação conforme necessário
+    // Configurar navegação
     const navigation = document.querySelector('.navigation');
     if (currentProject.images.length > 1) {
         navigation.style.display = 'flex';
@@ -246,7 +39,7 @@ function openPopup(projectId) {
     }
 }
 
-// Função para atualizar o conteúdo do popup
+// Atualizar o conteúdo do popup
 function updatePopupContent(image) {
     const popupImage = document.getElementById('popupImg');
     const popupTitle = document.getElementById('popupTitle');
@@ -332,5 +125,3 @@ function showSection(sectionId) {
     });
     document.getElementById(sectionId + 'Tab').classList.add('active');
 }
-
-
